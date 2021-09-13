fe0f1a17b2fcff6f0a33efc4aa0f23a5bfb1064b
dusaneshubham <56878960+dusaneshubham@users.noreply.github.com>
2021-09-13 23:13:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const Insert = require('./models/todo');
 
 require('./db/dbconn');
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || 80;
 
 const staticPath = path.join(__dirname, "../public");
 
